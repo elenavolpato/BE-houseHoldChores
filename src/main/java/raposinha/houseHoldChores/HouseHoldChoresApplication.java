07ed26029f6346c3ad39c7da9e0906c8c99b7cb2
raposinha <elena@raposinha.dev>
2026-05-06 17:30:01 +0200

    initial commit

---
+++ src/main/java/raposinha/houseHoldChores/HouseHoldChoresApplication.java
@@ -0,0 +1,13 @@
+package raposinha.houseHoldChores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseHoldChoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseHoldChoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raposinha/houseHoldChores/HouseHoldChoresApplicationTests.java
@@ -0,0 +1,13 @@
+package raposinha.houseHoldChores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHoldChoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
