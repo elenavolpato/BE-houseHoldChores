a574b477b727a65b577a880813e15abeee291430
raposinha <elena@raposinha.dev>
2026-05-08 15:06:43 +0200

    adds remove user from group and delete group - with groupadmin verification

--- src/main/java/raposinha/houseHoldChores/controller/GroupController.java
+++ src/main/java/raposinha/houseHoldChores/controller/GroupController.java
@@ -44,7 +44,18 @@ public class GroupController {
         return  userService.findByGroupId(groupId);
     }
 
+    @DeleteMapping("/delete/{groupId}")
+    public void deleteGroup(@PathVariable("groupId") String groupId, @RequestHeader("User-ID") UUID userId){
+        // todo: how to get the id from the loggedin user?
+        groupService.deleteGroup(groupId, userId);
+    }
+
     // remove user from group
+    @PatchMapping("/remove/members/{groupId}/{userToRemove}")
+    public ResponseEntity<String> removeUser(@PathVariable("groupId") String groupId,  @PathVariable("userToRemove") UUID userToRemove, @RequestHeader("User-ID") UUID requesterId){
+        String message = groupService.removeUserFromGroup(groupId, userToRemove, requesterId);
+        return ResponseEntity.ok(message);
+    }
 
     // see all groups members
 
--- src/main/java/raposinha/houseHoldChores/service/GroupService.java
+++ src/main/java/raposinha/houseHoldChores/service/GroupService.java
@@ -8,11 +8,13 @@ import raposinha.houseHoldChores.DTO.GroupResponseDTO;
 import raposinha.houseHoldChores.entities.Group;
 import raposinha.houseHoldChores.entities.User;
 import raposinha.houseHoldChores.entities.enums.GroupRole;
+import raposinha.houseHoldChores.exception.BadRequestException;
 import raposinha.houseHoldChores.exception.NotFoundException;
 import raposinha.houseHoldChores.exception.UnauthorizedException;
 import raposinha.houseHoldChores.repositories.GroupRepo;
 import raposinha.houseHoldChores.repositories.UserRepo;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -72,15 +74,56 @@ public class GroupService {
 
     }
     @Transactional
-    public void deleteGroup(String groupId, User requester) {
-        if (requester.getRole() != GroupRole.ADMIN) {
-            throw new UnauthorizedException("Only a group admin can delete the household!");
-        }
-        // ... delete logic todo
+        public void deleteGroup(String groupId, UUID requesterId) {
+        // check if logged in user is the admin of this group todo
+            User foundUser = userRepo.findById(requesterId)
+                    .orElseThrow(() -> new NotFoundException("User not found"));
+
+            Group foundGroup = groupRepo.findById(groupId)
+                    .orElseThrow(() -> new NotFoundException("Group not found"));
+
+            // only group admin/creator can delete group
+            if (foundUser.getRole() != GroupRole.ADMIN) {
+                throw new UnauthorizedException("Only a group admin can delete the household!");
+            }
+
+            //  Clear the relationship for ALL members
+            for (User member : foundGroup.getMembers()) {
+                member.setGroup(null);
+                member.setRole(null); // Reset their role too
+                userRepo.save(member);
+            }
+
+            groupRepo.delete(foundGroup);
+
     }
 
+    // remove user from group
+    @Transactional
+    public String removeUserFromGroup(String groupId, UUID userIdToRemove, UUID requesterId) {
+        User member = userRepo.findById(userIdToRemove)
+                .orElseThrow(() -> new NotFoundException("User to remove not found"));
+        User requester = userRepo.findById(requesterId)
+                .orElseThrow(() -> new NotFoundException("Requester not found"));
 
-    // remove user from group todo
+        // admin cannot remove themselves
+        if (userIdToRemove.equals(requesterId)) {
+            throw new BadRequestException("Admins cannot remove themselves. Please delete the group or transfer ownership first.");
+        }
+
+        // check if requester is admin of the group
+        if (requester.getRole() != GroupRole.ADMIN) {
+            throw new UnauthorizedException("Only the group admin can remove members.");
+        }
+
+        member.setGroup(null);
+        member.setRole(null); // Reset their power
+
+        userRepo.save(member);
+
+        return member.getUsername() + " was succesfully removed from group " + groupId;
+    }
+
 
     // filters by category - see what Giorgia did on the previous project todo
     // see all group tasks todo
